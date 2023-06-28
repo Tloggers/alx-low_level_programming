395fa0605d59a1ce379e44d17cf9f6bdb1b7d67d
Samuel Kamau <samuelkamau9110@gmail.com>
2023-06-27 17:53:03 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
   *puts2-Prints every other character of a string
   *@str:The string to be treated
   *Return:void
   */
+
 void puts2(char *str)
 {
 	int i;
@@ -17,3 +19,4 @@ void puts2(char *str)
 	_putchar(str[i]);
 	}
 	_putchar('\n');
+}
